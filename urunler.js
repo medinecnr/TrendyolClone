fdbb3f3eed7f089ea761908d5225477711b483ea
Medine <126980179+medinecnr@users.noreply.github.com>
2023-12-21 06:05:27 +0300

    Add files via upload

---
+++ urunler.js
@@ -0,0 +1,277 @@
+[
+    {
+        "title":"Siyah Pantolon",
+        "price":123,
+        "image":"./img/pantolon.jpg",
+        "id":"1"
+    },
+    {
+        "title":"Siyah Bol Paça Pantolon ",
+        "price":123,
+        "image":"./img/pantolon4.jpg",
+        "id":"2"
+    },
+    {
+        "title":"Siyah Dar Paça Pantolon",
+        "price":123,
+        "image":"./img/pantolon5.jpg",
+        "id":"3"
+    },
+    {
+        "title":"Siyah Boru Paça Pantolon",
+        "price":143,
+        "image":"./img/pantolon6.jpg",
+        "id":"4"
+    },
+
+    {
+        "title":"Yüksek Bel Pantolon",
+        "price":323,
+        "image":"./img/pantolon12.jpg",
+        "id":"10"
+    },
+    {
+        "title":"Kargo Cep Pantolon",
+        "price":223,
+        "image":"./img/pantolon13.jpg",
+        "id":"11"
+    },
+    {
+        "title":"Paraşüt Kumaş Pantolon",
+        "price":123,
+        "image":"./img/pantolon14.jpg",
+        "id":"12"
+    },
+    {
+        "title":"Paraşüt Kumaş Pantolon",
+        "price":123,
+        "image":"./img/pantolon15.jpg",
+        "id":"13"
+    },
+    {
+        "title":"Paraşüt Kumaş Pantolon",
+        "price":123,
+        "image":"./img/pantolon16.jpg",
+        "id":"14"
+    },
+    {
+        "title":"Paraşüt Kumaş Pantolon",
+        "price":123,
+        "image":"./img/pantolon17.jpg",
+        "id":"15"
+    },
+    {
+        "title":"Yeşil Pantolon",
+        "price":1223,
+        "image":"./img/pantolon7.jpg",
+        "id":"5"
+    },
+    {
+        "title":"Kahverengi Pantolon",
+        "price":23,
+        "image":"./img/pantolon8.jpg",
+        "id":"6"
+    },
+    {
+        "title":"Lacivert Pantolon",
+        "price":1223,
+        "image":"./img/pantolon9.jpg",
+        "id":"7"
+    },
+    {
+        "title":"Vizon Pantolon",
+        "price":323,
+        "image":"./img/pantolon10.jpg",
+        "id":"8"
+    },
+    {
+        "title":"Mint Pantolon",
+        "price":423,
+        "image":"./img/pantolon11.jpg",
+        "id":"9"
+    }, {
+        "title":"Siyah Pantolon",
+        "price":123,
+        "image":"./img/pantolon.jpg",
+        "id":"18"
+    },
+    {
+        "title":"Siyah Bol Paça Pantolon ",
+        "price":123,
+        "image":"./img/pantolon4.jpg",
+        "id":"19"
+    },
+    {
+        "title":"Siyah Dar Paça Pantolon",
+        "price":123,
+        "image":"./img/pantolon5.jpg",
+        "id":"20"
+    },
+    {
+        "title":"Siyah Boru Paça Pantolon",
+        "price":143,
+        "image":"./img/pantolon6.jpg",
+        "id":"21"
+    },
+
+    {
+        "title":"Yüksek Bel Pantolon",
+        "price":323,
+        "image":"./img/pantolon12.jpg",
+        "id":"22"
+    },
+    {
+        "title":"Kargo Cep Pantolon",
+        "price":223,
+        "image":"./img/pantolon13.jpg",
+        "id":"23"
+    },
+    {
+        "title":"Paraşüt Kumaş Pantolon",
+        "price":123,
+        "image":"./img/pantolon14.jpg",
+        "id":"24"
+    },
+    {
+        "title":"Paraşüt Kumaş Pantolon",
+        "price":123,
+        "image":"./img/pantolon15.jpg",
+        "id":"24"
+    },
+    {
+        "title":"Paraşüt Kumaş Pantolon",
+        "price":123,
+        "image":"./img/pantolon16.jpg",
+        "id":"25"
+    },
+    {
+        "title":"Paraşüt Kumaş Pantolon",
+        "price":123,
+        "image":"./img/pantolon17.jpg",
+        "id":"26"
+    },
+    {
+        "title":"Yeşil Pantolon",
+        "price":1223,
+        "image":"./img/pantolon7.jpg",
+        "id":"27"
+    },
+    {
+        "title":"Kahverengi Pantolon",
+        "price":23,
+        "image":"./img/pantolon8.jpg",
+        "id":"28"
+    },
+    {
+        "title":"Lacivert Pantolon",
+        "price":1223,
+        "image":"./img/pantolon9.jpg",
+        "id":" 29"
+    },
+    {
+        "title":"Vizon Pantolon",
+        "price":323,
+        "image":"./img/pantolon10.jpg",
+        "id":"30"
+    },
+    {
+        "title":"Mint Pantolon",
+        "price":423,
+        "image":"./img/pantolon11.jpg",
+        "id":"31"
+    },
+    {
+        "title":"Siyah Pantolon",
+        "price":123,
+        "image":"./img/pantolon.jpg",
+        "id":"46"
+    },
+    {
+        "title":"Siyah Bol Paça Pantolon ",
+        "price":123,
+        "image":"./img/pantolon4.jpg",
+        "id":"32"
+    },
+    {
+        "title":"Siyah Dar Paça Pantolon",
+        "price":123,
+        "image":"./img/pantolon5.jpg",
+        "id":"33"
+    },
+    {
+        "title":"Siyah Boru Paça Pantolon",
+        "price":143,
+        "image":"./img/pantolon6.jpg",
+        "id":"34"
+    },
+
+    {
+        "title":"Yüksek Bel Pantolon",
+        "price":323,
+        "image":"./img/pantolon12.jpg",
+        "id":"35"
+    },
+    {
+        "title":"Kargo Cep Pantolon",
+        "price":223,
+        "image":"./img/pantolon13.jpg",
+        "id":"36"
+    },
+    {
+        "title":"Paraşüt Kumaş Pantolon",
+        "price":123,
+        "image":"./img/pantolon14.jpg",
+        "id":"37"
+    },
+    {
+        "title":"Paraşüt Kumaş Pantolon",
+        "price":123,
+        "image":"./img/pantolon15.jpg",
+        "id":"38"
+    },
+    {
+        "title":"Paraşüt Kumaş Pantolon",
+        "price":123,
+        "image":"./img/pantolon16.jpg",
+        "id":"39"
+    },
+    {
+        "title":"Paraşüt Kumaş Pantolon",
+        "price":123,
+        "image":"./img/pantolon17.jpg",
+        "id":"40"
+    },
+    {
+        "title":"Yeşil Pantolon",
+        "price":1223,
+        "image":"./img/pantolon7.jpg",
+        "id":"41"
+    },
+    {
+        "title":"Kahverengi Pantolon",
+        "price":23,
+        "image":"./img/pantolon8.jpg",
+        "id":"42"
+    },
+    {
+        "title":"Lacivert Pantolon",
+        "price":1223,
+        "image":"./img/pantolon9.jpg",
+        "id":"43"
+    },
+    {
+        "title":"Vizon Pantolon",
+        "price":323,
+        "image":"./img/pantolon10.jpg",
+        "id":"44"
+    },
+    {
+        "title":"Mint Pantolon",
+        "price":423,
+        "image":"./img/pantolon11.jpg",
+        "id":"45"
+    }
+]
+
+
+       
